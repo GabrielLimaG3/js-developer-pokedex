619da2f596537dee1dce9236b9398d7da5ace524
Gabriel <ga.lima2205@gmail.com>
2024-10-28 21:08:37 -0300

    up

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -18,7 +18,7 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
+    return  fetch(pokemon.url)
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
 }
